04772626f14a3d900e0d5868c71b83f6cfa25077
Josegit <N00034975@upn.pe>
2025-04-30 16:51:53 -0500

    Agregar archivos de proyecto.

---
+++ Examen/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //Declaracion variables
+            int numero,d3,d2,d1;
+            //Ingreso datos
+            Console.WriteLine("Ingrese un numero de tres digitos");
+            numero = int.Parse(Console.ReadLine());
+            //Proceso
+            d1 = numero % 10;
+            Console.WriteLine("Digito 1: " + d1);
+            d2 = (numero/10) % 10;
+            Console.WriteLine("Digito 2: " + d2);
+            d3 = (numero / 100) % 10;
+            Console.WriteLine("Digito 3: " + d3);
+            //Salida de datos
+            Console.WriteLine("La suma de sus digitos son: " + (d1 + d2 + d3));
+            */
+
+            /*
+            //Declaracion variables
+            double monto;
+            Console.WriteLine("Ingrese un monto");
+            monto = int.Parse(Console.ReadLine());
+            //100,50,20,10,5,2,1,0.5,0.2,0.1
+            double b100 = 100, b50 = 50, b20 = 20, b10 = 10, m5 = 5, m2 = 2, m1 = 1, m0_5 = 0.5, m0_2 = 0.2, m0_1 = 0.1;
+            int cb100 = (int)(monto / b100);
+            monto = monto % b100;
+            int cb50 = (int)(monto / b50);
+            monto = monto % b50;
+            int cb20 = (int)(monto / b20);
+            monto = monto % b20;
+            int cb10 = (int)(monto / b10);
+            monto = monto % b10;
+            int cm5 = (int)(monto / m5);
+            monto = monto % cm5;
+            int cm2 = (int)(monto / m2);
+            monto = monto % cm2;
+            int cm1 = (int)(monto / m1);
+            monto = monto % cm1;
+            int cm0_5 = (int)(monto / m0_5);
+            monto = monto % cm0_5;
+            int cm0_2 = (int)(monto / m0_2);
+            monto = monto % cm0_2;
+            int cm0_1 = (int)(monto / m0_1);
+            monto = monto % cm0_1;
+            //Salida de datos
+            Console.WriteLine("Cantidad de billetes de 100: " + cb100);
+            Console.WriteLine("Cantidad de billetes de 50: " + cb50);
+            Console.WriteLine("Cantidad de billetes de 20: " + cb20);
+            Console.WriteLine("Cantidad de billetes de 10: " + cb10);
+            Console.WriteLine("Cantidad de monedas de 5: " + cm5);
+            Console.WriteLine("Cantidad de monedas de 2: " + cm2);
+            Console.WriteLine("Cantidad de monedas de 1: " + cm1);
+            Console.WriteLine("Cantidad de monedas de 0.5: " + cm0_5);
+            Console.WriteLine("Cantidad de monedas de 0.2: " + cm0_2);
+            Console.WriteLine("Cantidad de monedas de 0.1: " + cm0_1);
+            */
+
+            /*
+            decimal sueldo, impuesto, sueldoNeto;
+            Console.Write("Ingrese el sueldo del empleado: ");
+            sueldo = Convert.ToDecimal(Console.ReadLine());
+            if (sueldo <= 1500)
+            {
+                impuesto = sueldo * 0.10m;
+
+            }
+            else if (sueldo > 1500 && sueldo <= 3000)
+            {
+                impuesto = (sueldo - 1500) * 0.05m;
+            }
+            else
+            {
+                impuesto = (sueldo - 3000) * 0.03m;
+            }
+            sueldoNeto = sueldo - impuesto;
+
+            Console.WriteLine("El impuesto es: " + impuesto);
+
+            Console.WriteLine("El sueldo neto es: " + sueldoNeto);
+            */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
